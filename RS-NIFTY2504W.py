a98e470248d3936bd2aff9fa6ac3bfe43aba92d5
nikunj2021 <nikunj2021@gmail.com>
2026-08-08 16:47:04 +0530

    Update RS-NIFTY2504W.py

--- RS-NIFTY2504W.py
+++ RS-NIFTY2504W.py
@@ -23,7 +23,7 @@ INTER_DELAY = 0.5   # seconds between tickers
 
 # GOOGLE DRIVE CONFIGURATION
 # Replace this with the ID of the folder where you want the Excel files saved
-DRIVE_FILE_ID = "1Its-SSgeQQ1zNIKHm3VTx8hCRW2SBkKpLYR1IC3l4Tw"
+DRIVE_FILE_ID = "1si998YOD2LryRwZmUwgvVtK_wjtqfoO5"
 
 # ─────────────────────────────────────────────
 # FETCH ONE TICKER — let yfinance manage session
